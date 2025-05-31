8a78a2c3112795bd1b0477d2271e7464aa9e539e
joaolira00 <joao134.jol@gmail.com>
2025-05-30 21:08:19 -0300

    feat: adds appointment route to main

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import models.pacient_model as pacient_model
 from database.database import engine
 from scalar_fastapi import get_scalar_api_reference
 from auth import auth
-from routers import pacients
-
+from routers import pacients, appointments
 
 app = FastAPI()
 
@@ -12,6 +11,7 @@ pacient_model.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(pacients.router)
+app.include_router(appointments.router)
 
 @app.get("/scalar", include_in_schema=False)
 async def scalar_html():
