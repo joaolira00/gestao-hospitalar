326e00b8f7bb87535e5f4e22a0389126951673ef
joalira00 <joao134.jol@gmail.com>
2025-05-30 22:51:41 -0300

    feat: catches execpetion when .env with database connection string is missing or null

--- database/database.py
+++ database/database.py
@@ -9,6 +9,11 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not SQLALCHEMY_DATABASE_URL:
+    raise RuntimeError("A variável DATABASE_URL está vazia ou não foi definida. "
+                       "Verifique o seu arquivo .env e defina corretamente a string de conexão.")
+
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
